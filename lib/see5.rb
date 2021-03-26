77b1e4c68420388da6d8594e4df73c5e1b962c48
Eddie Lebow <elebow@users.noreply.github.com>
2021-03-26 01:27:13 -0400

    Attempt to run executables from see5-installer gem

--- lib/see5.rb
+++ lib/see5.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "bundler"
+
 require "see5/input_file_writer"
 require "see5/model"
 require "see5/rules_output_parser"
@@ -30,10 +32,24 @@ module See5
   end
 
   def self.run_see5
-    system("c5.0 -f /tmp/ruby-see5 -r > /tmp/ruby-see5.rules_output")
+    system("#{see5_executable} -f /tmp/ruby-see5 -r > /tmp/ruby-see5.rules_output")
   end
 
   def self.run_gritbot
-    system("gritbot -s -f /tmp/ruby-see5 -r > /tmp/ruby-see5.gritbot_output")
+    system("#{gritbot_executable} -s -f /tmp/ruby-see5 -r > /tmp/ruby-see5.gritbot_output")
+  end
+
+  private
+
+  private_class_method def self.see5_executable
+    @see5_executable ||= see5_installer_path ? "#{see5_installer_path}/ext/c5.0/c5.0" : "c5.0"
+  end
+
+  private_class_method def self.gritbot_executable
+    @gritbot_executable ||= see5_installer_path ? "#{see5_installer_path}/ext/gritbot/gritbot" : "gritbot"
+  end
+
+  private_class_method def self.see5_installer_path
+    @see5_installer_path ||= Bundler.rubygems.find_name("see5-installer").first&.full_gem_path
   end
 end
